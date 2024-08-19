28457f4a77ba29136f08042d80997aaf5aa1d50a
Chain Platform <support@chainplatform.net>
2024-08-19 13:00:53 +0700

    Add Admin Layout setting.

---
+++ LayoutAdmin.js
@@ -0,0 +1,31 @@
+export function getLayoutAdmin(layoutWidth, isDesktop, side_bar_width, styles) {
+    let numberChart = 1;
+    let padding = styles.s30;
+    let sideBarWidth = side_bar_width;
+    let sideBarOpen = false;
+    let sideBarMiniWidth = styles.s70;
+    let sideBarSecondWidth = side_bar_width - styles.s70;
+    if (isDesktop && layoutWidth > styles.base_desktop) {
+        sideBarWidth = side_bar_width;
+    }
+    if (layoutWidth > 600 && layoutWidth < styles.base_desktop) {
+        numberChart = 2;
+        padding = styles.s45;
+    }
+    if (layoutWidth >= styles.base_desktop && layoutWidth < styles.split_desktop) {
+        numberChart = 3;
+        padding = styles.s60;
+    }
+    if (layoutWidth >= styles.split_desktop) {
+        numberChart = 4;
+        padding = styles.s75;
+    }
+    const chartWidth = (layoutWidth - sideBarMiniWidth - padding) / numberChart;
+    let view = {};
+    view.sideBarWidth = sideBarWidth;
+    view.sideBarMiniWidth = sideBarMiniWidth;
+    view.sideBarSecondWidth = sideBarSecondWidth;
+    view.sideBarOpen = sideBarOpen;
+    view.chartWidth = chartWidth;
+    return view;
+}
